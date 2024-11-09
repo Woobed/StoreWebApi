7c155afe2f5a7e6d4a1faf748be3c736043a8a09
Woobed <nshindyapin@bk.ru>
2024-11-09 16:44:11 +0300

    Gitignore added

---
+++ Core/Domain/Order.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Domain
+{
+    internal class Order
+    {
+        public Guid Id { get; set; }
+        public Guid UserId { get; set; }
+        public string? Adress { get; set; }
+        public string? Note { get; set; }
+        public DateTime? Created { get; set; }
+
+        public Order() {}
+        public Order(Guid userId, string adress, string note,DateTime created)
+        {
+            Id = userId;
+            Adress = adress;
+            Note = note;
+            Created = created;
+        }
+
+    }
+}
