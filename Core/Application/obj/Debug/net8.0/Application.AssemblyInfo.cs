622e3cc3777b226de8c69cdf847e14489d4fb6a1
Woobed <nshindyapin@bk.ru>
2024-11-09 17:30:12 +0300

    Mapping interface has been added

--- Core/Application/obj/Debug/net8.0/Application.AssemblyInfo.cs
+++ Core/Application/obj/Debug/net8.0/Application.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Application")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+7c155afe2f5a7e6d4a1faf748be3c736043a8a09")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+664cf8d9c0f6390329b8514090da84a085f1163f")]
 [assembly: System.Reflection.AssemblyProductAttribute("Application")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Application")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
